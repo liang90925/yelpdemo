6cf7faeea6cbe93b16661f62094fd479dc95273c
Yunpeng Liu <PengLiang@YunpengdeiMac.local>
2015-07-30 00:13:48 -0700

    add searchkick

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem "fog"
 gem "figaro"
 gem "mini_magick"
 gem 'devise'
+gem 'searchkick'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
  
   devise_for :users
   resources :restaurants do
-     resources :reviews, except: [:show, :index]
+    collection do
+      get 'search'
+    end
+    resources :reviews, except: [:show, :index]
   end
-  
+
   get 'pages/about'
 
   get 'pages/contact'
