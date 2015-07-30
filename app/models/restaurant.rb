b8e87f96caba2096b4d0d5660abaef8797debf5f
Yunpeng Liu <PengLiang@YunpengdeiMac.local>
2015-07-29 23:34:43 -0700

    set user permissions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,13 +4,13 @@ class Restaurant < ActiveRecord::Base
 
 	validates :name, :address, :phone, :website, :image, presence: true
 
-	validates :address, format: { with: /https?:\/\/.*\z/,
+	validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
     message: "Must be in the format 350 Fifth Avenue, New York, NY 10118" }
 
 	validates :phone, format: { with: /\(\d{3}\) \d{3}-\d{4}\z/,
     message: "Must be in the format (123) 456-7890" }
 
-    validates :website, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    validates :website, format: { with: /https?:\/\/.*\z/,
     message: "Must start with http:// or https://" }
     
     
